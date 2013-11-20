a20e20558663bb58f0bb1653a7d610c41da9854d
arona12 <arona12@ru.is>
2013-11-20 21:34:35 +0000

    updated TicTacToe.java & TicTacToeTest.java

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -12,13 +12,44 @@ public class TicTacToe{
 		this.player = true;
 		this.movesLeft = 9;
 		this.table = new int[3][3];
-		//this.genTable();
+		this.genTable();
 	}
 
 	//Sets up the table for new game
-	/*public void genTable(){
+	public void genTable(){
 		for(int row = 0; row < 3; row++){
-			for(int column = 0; column < 3)
+			for(int column = 0; column < 3; column++)
+				this.table[row][column] = -3;
 		}
-	}*/
+	}
+
+	//Returns the int value player should insert based on the value of player
+	public int setMarker(){
+		if(player)	{ return 1; }
+		else		{ return 0; }
+	}
+
+	//Returns true if move was made, otherwise false
+	public boolean makeMove(int chosenSquare){
+		int counter = 1;
+		int marker = this.setMarker();
+
+		if(chosenSquare < 1 || chosenSquare > 9){ return false; }
+
+		for(int row = 0; row < 3; row++){
+			for(int column = 0; column < 3; column++){
+				if(counter == chosenSquare){
+					if(this.table[row][column] == -3){
+						this.table[row][column] = marker;
+						return true;
+					}
+					else{
+						return false;
+					}
+				}
+				counter++;
+			}
+		}
+		return false;
+	}
 }
--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -4,10 +4,11 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TicTacToeTest{
+	TicTacToe game;
 
 	@Test
 	public void constructorTest(){
-		TicTacToe game = new TicTacToe();
+		game = new TicTacToe();
 
 		//player
 		assertEquals("player should be set to true and initialization", true, game.player);
@@ -21,6 +22,84 @@ public class TicTacToeTest{
 		//table columns
 		for(int row = 0; row < 3; row++){
 			assertEquals("table should have 3 columns", 3, game.table[row].length);
+		}
+	}
+
+	@Test
+	public void genTableTest(){
+		TicTacToe game = new TicTacToe();
+		game.genTable();
+
+		for(int row = 0; row < 3; row++){
+			for(int column = 0; column < 3; column++){
+				assertEquals("The table array should be filled with all -3", -3, game.table[row][column]);
+			}
+		}
+	}
+
+	@Test
+	public void setMarkerTest(){
+		game = new TicTacToe();
+
+		assertEquals("Player is initialized to true, so setMarker should return 1", 1, game.setMarker());
+		game.player = false;
+		assertEquals("Player is false, so setMarker should return 0", 0, game.setMarker());
+	}
+
+	@Test
+	public void makeMoveTest(){
+		game = new TicTacToe();
+
+		//Input must be 1-9, including 1 and 9
+		assertEquals("There is no square 0", false, game.makeMove(0));		
+		assertEquals("There is no square 10", false, game.makeMove(10));
+		assertEquals("Square 1 is free", true, game.makeMove(1));
+		assertEquals("Square 1 is now occupied", false, game.makeMove(1));
+		assertEquals("Square 2 is free", true, game.makeMove(2));
+		assertEquals("Square 2 is now occupied", false, game.makeMove(2));
+		assertEquals("Square 3 is free", true, game.makeMove(3));
+		assertEquals("Square 3 is now occupied", false, game.makeMove(3));
+		assertEquals("Square 4 is free", true, game.makeMove(4));
+		assertEquals("Square 4 is now occupied", false, game.makeMove(4));
+		assertEquals("Square 5 is free", true, game.makeMove(5));
+		assertEquals("Square 5 is now occupied", false, game.makeMove(5));
+		assertEquals("Square 6 is free", true, game.makeMove(6));
+		assertEquals("Square 6 is now occupied", false, game.makeMove(6));
+		assertEquals("Square 7 is free", true, game.makeMove(7));
+		assertEquals("Square 7 is now occupied", false, game.makeMove(7));
+		assertEquals("Square 8 is free", true, game.makeMove(8));
+		assertEquals("Square 8 is now occupied", false, game.makeMove(8));
+		assertEquals("Square 9 is free", true, game.makeMove(9));
+		assertEquals("Square 9 is now occupied", false, game.makeMove(9));
+	}
+
+	@Test
+	public void findWinnerTest(){
+		game = new TicTacToe();
+		assertEquals("Empty table", false, game.findWinner());
+
+		game.makeMove(1);
+		game.makeMove(3);
+		game.makeMove(4);
+		game.makeMove(6);
+		game.makeMove(8);
+
+		assertEquals("No 3 in a row", false, game.findWinner());
+
+		game.genTable();
+		for(int row = 0; row < 3; row+=3){
+			game.makeMove(row + 1);
+			game.makeMove(row + 2);
+			game.makeMove(row + 3);
+			assertEquals(true, game.findWinner());
+			game.genTable();
+		}
+		for(int column = 0; column < 3; column+=3){
+			game.makeMove(column + 1);
+			game.makeMove(column + 4);
+			game.makeMove(column + 7);
+			assertEquals(true, game.findWinner());
+			game.genTable();
 		}
 	}
 }
