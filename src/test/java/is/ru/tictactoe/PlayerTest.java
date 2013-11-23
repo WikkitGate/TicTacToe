db7506a108185e60f6e33e5f512300a33f62780d
oli <regnbogasulta@gmail.com>
2013-11-23 22:37:56 +0000

    setti inn beinagrind af PlayerTest.java

---
+++ src/test/java/is/ru/tictactoe/PlayerTest.java
@@ -0,0 +1,17 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class PlayerTest.java{
+
+	@Test
+	public void constructorTest(){
+		
+	}
+
+	@Test
+	public int testElo(){
+		
+	}
+}
