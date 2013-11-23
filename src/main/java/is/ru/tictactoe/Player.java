43312b14b048ab055a56cf83da588ac55b9b098f
arona12 <arona12@ru.is>
2013-11-23 23:42:23 +0000

    Updated Player.java

--- src/main/java/is/ru/tictactoe/Player.java
+++ src/main/java/is/ru/tictactoe/Player.java
@@ -5,7 +5,7 @@ public class Player{
 	private int wins;
 	private int losses;
 	private float elo;
-	private int playerInt;
+	private boolean playerValue;
 
 	//Constructor takes in name and sets Wins, losses and elo to 0
 	public Player(){
