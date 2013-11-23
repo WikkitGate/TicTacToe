3bc6507bc989dbbd15506c0712be45dd98595a02
oli <regnbogasulta@gmail.com>
2013-11-23 22:13:21 +0000

    bætti við beinagrind af Player.java

---
+++ src/main/java/is/ru/tictactoe/Player.java
@@ -0,0 +1,58 @@
+package is.ru.tictactoe;
+
+public class Player{
+	private String name;
+	private int wins;
+	private int losses;
+	private float elo;
+
+	//Constructor takes in name and sets Wins, losses and elo to 0
+	public Player(name){
+		
+	}
+
+	//returns the name of the current Player
+	public String getName(){
+		return "";
+	}
+
+	//Change the Name
+	public void setName(){
+
+	}
+
+	//returns the number of wins won by the current player
+	public int getWins(){
+		return 0;
+	}
+
+	//Change the wins
+	public void setWins(){
+
+	}
+
+	//get the number of losses
+	public int getLosses(){
+		return 0;
+	}
+
+	//change the Losses
+	public void setLosses(){
+
+	}
+
+	//get the Elo
+	public float getElo(){
+		return 0;
+	}
+
+	//change the Elo
+	public void setElo(){
+
+	}
+
+	//Calculates the Elo
+	public calculateElo(){
+
+	}
+}
