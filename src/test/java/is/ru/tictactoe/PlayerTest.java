19e80c0b9508c30f1c3f7f76b81141e7cb94dcd9
arona12 <arona12@ru.is>
2013-11-24 00:38:22 +0000

    Update: PlayerTest.java

--- src/test/java/is/ru/tictactoe/PlayerTest.java
+++ src/test/java/is/ru/tictactoe/PlayerTest.java
@@ -4,41 +4,44 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class PlayerTest{
-	Player player;
+	Player player = new Player("Nonni", true);
 
 	@Test
 	public void constructorTest(){
-		player = new player("Einar", false);
-
-		assertEquals("player should be set to true and initialization", true, game.getPlayer());
+		assertEquals("Name at this point should be Nonni", "Nonni", player.getName());
+		assertEquals("Wins should be initialized to 0", 0, game.getWins());
+		assertEquals("PlayerValue should be true at this point", true, player.getPlayerValue());
 	}
 
-	//	setName() and getName() tested together
+	//Test unit for setName() and getName()
 	@Test
 	public void nameTest(){
-		player = new player("Sveeeeinn", false);
-		assertEquals("This should return an empty string, since the name has not been set yet", "", player.getName());
+		player.setName("Siddi");
+		assertEquals("setName should have set the name to Siddi", "Siddi", player.getName());
 		player.setName("Einar")
-		assertEquals("This should return the name Einar", "Einar", player.getName());
+		assertEquals("setName should have set the name to Einar", "Einar", player.getName());
 
 	}
 
 	@Test
 	public void winsTest(){
-		player = new player("Einar", false);
-
-		assertEquals("This should return 0, since the player hasn't won any games", 0, player.getWins());
 		player.setWins(4);
-		assertEquals("This hould return 4", 4, player.getWins());
+		assertEquals("Wins have been set to 4", 4, player.getWins());
+		player.setWins(5);
+		assertEquals("Wins have been set to 5", 5, player.getWins());
+		player.setWins(3);
+		assertEquals("Wins have been set to 3", 3, player.getWins());
+		player.setWins(12);
+		assertEquals("Wins have been set to 12", 12, player.getWins());
 	}
 
 	@Test
 	public void playerValueTest(){
-		player = new player("Einar", false);
-		assertEquals("This should return false", false, player.getPlayerValue());
+		player.setPlayerValue(false);
+		assertEquals("PlayerValue has been set to true", false, player.getPlayerValue());
 
 		player.setPlayerValue(true);
-		assertEquals("This should return true", true, player.getPlayerValue());
+		assertEquals("PlayerValue has been set to true", true, player.getPlayerValue());
 
 	}
 }
